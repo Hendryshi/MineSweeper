c3e9478986742ded57f9c68d33a779923e5bba47
YEJIA SHI <yejia.shi@hotmail.com>
2020-04-05 23:48:57 +0200

    Add project files.

---
+++ MineSweeper/mainForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace MineSweeper
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.gameGToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameNToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainMenuStrip
+            // 
+            this.mainMenuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameGToolStripMenuItem});
+            this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mainMenuStrip.Name = "mainMenuStrip";
+            this.mainMenuStrip.Size = new System.Drawing.Size(1231, 28);
+            this.mainMenuStrip.TabIndex = 0;
+            this.mainMenuStrip.Text = "menuStrip1";
+            // 
+            // gameGToolStripMenuItem
+            // 
+            this.gameGToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameNToolStripMenuItem});
+            this.gameGToolStripMenuItem.Name = "gameGToolStripMenuItem";
+            this.gameGToolStripMenuItem.Size = new System.Drawing.Size(86, 24);
+            this.gameGToolStripMenuItem.Text = "Game(&G)";
+            // 
+            // newGameNToolStripMenuItem
+            // 
+            this.newGameNToolStripMenuItem.Name = "newGameNToolStripMenuItem";
+            this.newGameNToolStripMenuItem.Size = new System.Drawing.Size(193, 26);
+            this.newGameNToolStripMenuItem.Text = "New Game(&N)";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1231, 702);
+            this.Controls.Add(this.mainMenuStrip);
+            this.MainMenuStrip = this.mainMenuStrip;
+            this.Name = "mainForm";
+            this.Text = "MineSweeper";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.mainForm_Paint);
+            this.mainMenuStrip.ResumeLayout(false);
+            this.mainMenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem gameGToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameNToolStripMenuItem;
+    }
+}
+
---
+++ MineSweeper/mainForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MineSweeper
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void mainForm_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
